c49643f9f6861a33e75a7e9ba3b51297779584fd
DylanC3 <dylancho34@gmail.com>
2021-10-22 15:04:01 -0700

    sample task

--- backend.js
+++ backend.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const tasks = {
+    task_list :
+    [
+       {
+          id : 'xyz789',
+          note : 'do homework',
+          month_due: '10',
+          day_due: '30'
+       }
+    ]
+ }
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
