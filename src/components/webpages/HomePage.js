f66941814d77107191bb31926cf9b9bf54523b27
Evelyn Tran <etran53@calpoly.edu>
2021-11-28 01:24:08 -0800

    add additional files

---
+++ src/components/webpages/HomePage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './HomePage.css'; 
+
+function HomePage() {
+    return (
+        <div className='container'>
+            <h1 className='hook'>CUT OUT THE BS</h1>
+            <div className='row'>
+                <div className='col'>
+                    <div className='text'>
+                        <h1 className='header'>a simple to-do list designed to put you at ease</h1>
+                        <Link to='/myTodoList'>
+                        <button className='start-button'>Get Started</button>
+                        </Link>
+                    </div>
+                </div> 
+                <div className='col'>
+                    <div className='image'>
+                        <img src='images/img-2.jpg' alt='To-do list clipart' className='home-image' align='left'/>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    );
+}
+
+export default HomePage; 
