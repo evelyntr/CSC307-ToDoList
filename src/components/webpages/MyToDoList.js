0ad2d0235ce0d0f5f32db936611eb2fb33775628
Evelyn Tran <etran53@calpoly.edu>
2021-12-01 15:49:44 -0800

    add, delete, update list

--- src/components/webpages/MyToDoList.js
+++ src/components/webpages/MyToDoList.js
@@ -1,43 +1,134 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MyToDoList.css';
+import TodoForm from "../TodoForm";
+import TodoList from "../TodoList";
+import { VscEllipsis, VscClose } from 'react-icons/vsc';
+// import { GoThreeBars } from 'react-icons/go';
+// import { IoClose } from 'react-icons/io5';
+// import { FiInfo } from 'react-icons/fi';
 
-const intialList = [
-    {
-        title: 'Today',
-        numTasks: 0,
-    },
-];
+// const intialList = [
+//     {
+//         listName: 'Today',
+//         numTasks: 0,
+//     },
+// ];
 
 function MyToDoList() {
-    
+    const [click, setClick] = useState(false);
+
+    // const showAllLists = () => setClick(!click);
+    const handleClick = () => setClick(!click);
+    const closeMenu = () => setClick(false);
+    const handleClearTasks = () => {
+        closeMenu();
+        setClick(!click);
+    };
+
+    const handleDeleteList = () => {
+        closeMenu();
+        setClick(!click);
+    };
+
+    const [tasks, setTasks] = useState([]);
+
+  function addTask(task) {
+    setTasks([task, ...tasks]);
+  }
+
+  function toggleComplete(id) {
+    setTasks(
+      tasks.map(task => {
+        if (task.id === id) {
+          return {
+            ...task,
+            completed: !task.completed
+          };
+        }
+        return task;
+      })
+    )
+  }
+
+  function deleteTask(id) {
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
     return (
         <div className='container'>
             <div className='row'>
-                <div className='col'>
+                <div className='panel'>
                     <div className='all-lists'>
-                        <h1 className='all-lists-title'>My Lists</h1>
-                        <ul className='bullet-lists'>
-                            {intialList.map(item => (
-                                <li className='current-list'>{item.title}
+                        {/* <div className='all-lists-icon' onClick={showAllLists}>
+                            {click ? <IoClose /> : <GoThreeBars />}
+                        </div> */}
+                        <h2 className='all-lists-title'>My Lists</h2>
+                        <ul className='list-of-lists' data-lists>
+                            <li className='list-name active'>Today </li>
+                            <li className='list-name'>Tomorrow</li>
+                            
+                            
+                            {/* {intialList.map(item => (
+                                <p className='current-list'>{item.listName}
                                     <p className='num-tasks'>{item.numTasks}</p>
-                                </li>
-                            ))}
+                                </p>
+                            ))} */}
                             
                         </ul>
-                        <form >
+                        <form className='add-list-form' action='' data-new-list-form>
                             <input
                                 placeholder='add list'
                                 type='text'
-                                className='new-list'
+                                data-new-list-input
                             />
                             <button className='list-button'>+</button>
                         </form>
                     </div>
                 </div>
-                <div className='col'>
+                <div className='main'>
                     <div className='single-list'>
-                        <h2 className='single-list-title'>Today</h2>
-                        <p className='num-tasks'>0</p>
+                        <div className='single-list-header'>
+                            <h1 className='single-list-title'>Today</h1>
+                            <p className='num-tasks'>0</p>
+                    
+                            {/* {intialList.map(item => (
+                                <h1 className='single-list-title'>{item.listName}
+                                    <p className='num-tasks'>{item.numTasks}</p>
+                                </h1>
+                            ))} */}
+                 
+                            <div className='dropdown-icon' onClick={handleClick}>
+                                {click ? <VscClose /> : <VscEllipsis />}
+                            </div>
+                            <ul className={click ? 'dropdown-menu-active' : 'dropdown-menu'}>
+                                <li className='menu-item-1' onClick={handleClearTasks}>Clear completed tasks</li>
+                                <li className='menu-item-2' onClick={handleDeleteList}>Delete list</li>
+                            </ul>
+                        </div>
+                        
+                        <div className='single-list-tasks'>
+                            <div className='tasks' data-tasks />
+                            <TodoList
+                                tasks={tasks} 
+                                toggleComplete={toggleComplete}
+                                deleteTask={deleteTask}>
+                            </TodoList>
+                            <TodoForm addTask={addTask}/>
+                            {/* <template className='task-template'>
+                                <div className='task'>
+                                    <input type='checkbox' />
+                                    <span className='custom-checkbox' />
+                                </div>
+                            </template>
+                            <form className='add-task-form' action='' data-new-task-form>
+                                <input
+                                    placeholder='add task'
+                                    type='text'
+                                    data-new-task-input
+                                />
+                                <button className='task-button'>+</button>
+                            </form> */}
+                        </div>
                     </div>
                 </div>
             </div>
