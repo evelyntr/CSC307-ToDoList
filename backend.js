a425cea759d69cc21d0810ca3a4cf039509a9f32
vickymlam <vickylam780@gmail.com>
2021-12-01 15:29:26 -0800

    changed urls

--- backend.js
+++ backend.js
@@ -9,49 +9,39 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("To Do List!");
-});
+// app.get("/", (req, res) => {
+//   res.send("To Do List!");
+// });
 
-app.get("/tasks", async (req, res) => {
+app.get("/", async (req, res) => {
   const name = req.query["name"];
   try {
     const result = await taskServices.getTasks(name);
-    res.send({ taskList: result });
+    res.send({ task_list: result });
   } catch (error) {
     console.log(error);
     res.status(500).send("An error ocurred in the server.");
   }
 });
 
-app.get("/tasks/:listName", async (req, res) => {
+app.get("/:listName", async (req, res) => {
   const listName = req.params["listName"];
   const result = await taskServices.sortList(listName);
   if (result === undefined || result === null)
     res.status(404).send("Resource not found.");
   else {
-    res.send({ taskList: result });
+    res.send({ task_list: result });
   }
 });
 
-app.get("/tasks/:listName/:id", async (req, res) => {
-  const id = req.params["id"];
-  const result = await taskServices.findTaskById(id);
-  if (result === undefined || result === null)
-    res.status(404).send("Resource not found.");
-  else {
-    res.send({ taskList: result });
-  }
-});
-
-app.post("/tasks", async (req, res) => {
+app.post("/", async (req, res) => {
   const task = req.body;
   const savedTask = await taskServices.addTask(task);
   if (savedTask) res.status(201).send(savedTask);
   else res.status(500).end();
 });
 
-app.delete("/tasks/:id", async (req, res) => {
+app.delete("/:id", async (req, res) => {
   /* works to delete via id */
   const id = req.params["id"];
   const result = await taskServices.findTaskById(id);
