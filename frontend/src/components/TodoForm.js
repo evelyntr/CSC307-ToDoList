1ac6a24fa10fb72a5025b3f92c7f2d8d42b422d8
DylanC3 <dylancho34@gmail.com>
2021-12-05 14:23:57 -0800

    push update

--- frontend/src/components/TodoForm.js
+++ frontend/src/components/TodoForm.js
@@ -1,40 +1,42 @@
 import React, { useState } from "react";
 
-function TodoForm({addTask}) {
-    const [task, setTask] = useState({
-        id: "",
-        name: "",
-        completed: false,
-        priority: 0
-    });
+function TodoForm({ addTask }) {
+  const [task, setTask] = useState({
+    id: "",
+    name: "",
+    completed: false,
+    priority: 0,
+  });
 
-    function handleTaskInputChange(e) {
-        setTask({ ...task, name: e.target.value });
-    }
+  function handleTaskInputChange(e) {
+    setTask({ ...task, name: e.target.value });
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        if (task.name.trim()) {
-            const crypto = require("crypto");
-            const id = crypto.randomBytes(16).toString("hex");
-            addTask({ ...task, id: id });
-            // reset task input 
-            setTask({...task, name: ""});
-        }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (task.name.trim()) {
+      const crypto = require("crypto");
+      const id = crypto.randomBytes(16).toString("hex");
+      addTask({ ...task, id: id });
+      // reset task input
+      setTask({ ...task, name: "" });
     }
-    return (
-        <form onSubmit={handleSubmit}>
-            <input 
-                align="center"
-                placeholder="add task"
-                name="task"
-                type="text"
-                value={task.name}
-                onChange={handleTaskInputChange}
-                />
-            <button size="lg" type="add">+</button>
-        </form>
-    )
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        align="center"
+        placeholder="add task"
+        name="task"
+        type="text"
+        value={task.name}
+        onChange={handleTaskInputChange}
+      />
+      <button size="lg" type="add">
+        +
+      </button>
+    </form>
+  );
 }
 
 export default TodoForm;
