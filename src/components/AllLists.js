c53ecaa568ae23a52ad14edb2c9ca4d619386096
Evelyn Tran <etran53@calpoly.edu>
2021-12-05 22:36:24 -0800

    list files

---
+++ src/components/AllLists.js
@@ -0,0 +1,18 @@
+import React from "react";
+import MyList from "./MyList";
+
+function AllLists({ lists, setActive, deleteList }) {
+    return (
+        <ul>
+            {lists.map(list => (
+                <MyList 
+                    key={list.id} 
+                    list={list} 
+                    setActive={setActive}
+                    deleteList={deleteList}></MyList>
+            ))}
+        </ul>
+    );
+}
+
+export default AllLists; 
